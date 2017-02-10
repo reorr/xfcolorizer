baba4ae35e4defa000117d86b881ff67bd04e21f
reorr <reorr@github.com>
2017-02-10 16:14:28 +0700

    fist commit, not fix yet

---
+++ color.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+directory=$HOME'/.themes/xfcolorize/'
+
+last_window_color=1F4634
+last_ancent_color=BB6368
+last_ancent2_color=4E9B71
+
+new_window_color=$1
+new_ancent_color=$2
+new_ancent2_color=$3
+
+INKSCAPE=/usr/bin/inkscape
+SVG="$directory"gtk-2.0/assets_fix.svg
+LISTFILE="$directory"gtk-2.0/assets.txt
+SRC_FILE="$directory"gtk-3.0/assets_fix.svg
+ASSETS_DIR="$directory"gtk-3.0/assets
+ASSETS_DIR20="$directory"gtk-3.20/assets
+ASSETS_DIR22="$directory"gtk-3.22/assets
+INDEX="$directory"gtk-3.0/assets.txt
+WM_LIST="$directory"xfwm4/list.txt
+RM_ASSETS_GTK_3="$directory"gtk-3.0/rm_assets.txt
+RM_ASSETS_GTK_2="$directory"gtk-2.0/rm_assets.txt
+
+echo Changing window color
+find "$directory"xfwm4/ -type f -exec sed -i s/"$last_window_color"/"$new_window_color"/g {} \;
+
+echo Changing GTK ancent color
+find "$directory"gtk-2.0/ -type f -exec sed -i s/"$last_window_color"/"$new_window_color"/g {} \;
+sed -i s/"$last_ancent_color"/"$new_ancent_color"/g "$directory"gtk-3.0/gtk.css
+sed -i s/"$last_ancent_color"/"$new_ancent_color"/g "$directory"gtk-3.20/gtk.css
+sed -i s/"$last_ancent_color"/"$new_ancent_color"/g "$directory"gtk-3.22/gtk.css
+sed -i s/"$last_ancent_color"/"$new_ancent_color"/g "$directory"gtk-2.0/assets_fix.svg
+sed -i s/"$last_ancent_color"/"$new_ancent_color"/g "$directory"gtk-3.0/assets_fix.svg
+
+echo Changing termite background color
+sed -i s/"$last_window_color"/"$new_window_color"/g "$HOME"/.config/termite/config
+
+echo Changing wishker menu color
+sed -i s/"$last_window_color"/"$new_window_color"/g "$HOME"/.gtkrc-2.0.mine
+sed -i s/"$last_ancent_color"/"$new_ancent_color"/g "$HOME"/.gtkrc-2.0.mine
+
+echo Changing notification color
+find "$directory"xfce-notify-4.0/ -type f -exec sed -i s/"$last_window_color"/"$new_window_color"/g {} \;
+find "$directory"xfce-notify-4.0/ -type f -exec sed -i s/"$last_ancent_color"/"$new_ancent_color"/g {} \;
+find "$directory"xfce-notify-4.0/ -type f -exec sed -i s/"$last_ancent2_color"/"$new_ancent2_color"/g {} \;
+
+for wmwm in `cat $WM_LIST`
+do
+	sed -i s/"$last_window_color"/"$new_window_color"/g "$directory"xfwm4/$wmwm
+done
+
+sed -i s/"$last_window_color"/"$new_window_color"/g color.sh
+sed -i s/"$last_ancent_color"/"$new_ancent_color"/g color.sh
+sed -i s/"$last_ancent2_color"/"$new_ancent2_color"/g color.sh
+
+echo Rendering assets
+for assets_del in `cat $RM_ASSETS_GTK_3`
+do
+	rm "$ASSETS_DIR"/"$assets_del".png
+	rm "$ASSETS_DIR"/"$assets_del"@2.png
+	rm "$ASSETS_DIR20"/"$assets_del".png
+	rm "$ASSETS_DIR20"/"$assets_del"@2.png
+	rm "$ASSETS_DIR22"/"$assets_del".png
+	rm "$ASSETS_DIR22"/"$assets_del"@2.png
+done
+
+for del_assets in `cat $RM_ASSETS_GTK_2`
+do
+	rm "$directory"gtk-2.0/$del_assets.png
+done
+
+for filename in `cat $LISTFILE`
+do
+	DIR=`echo $filename | cut -f1 -d '/'`
+    echo Rendering "$directory"gtk-2.0/"$filename".png
+	ID=`echo $filename | tr '/' '_'`
+	$INKSCAPE $SVG --export-id=$ID -e "$directory"gtk-2.0/"$filename".png &>/dev/null
+	#fi
+done
+
+rm -rf "$ASSETS_DIR20"/*
+rm -rf "$ASSETS_DIR22"/*
+
+for i in `cat $INDEX`
+do 
+    echo Rendering $ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE &>/dev/null #\
+    echo Rendering $ASSETS_DIR/$i@2.png
+    $INKSCAPE --export-id=$i \
+              --export-dpi=180 \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE &>/dev/null #\
+done
+
+cp "$ASSETS_DIR"/* "$ASSETS_DIR20"/
+cp "$ASSETS_DIR"/* "$ASSETS_DIR22"/
+
+echo Appliying theme
+xfconf-query -c xfwm4 -p /general/theme -s "adwaita"
+xfconf-query -c xfwm4 -p /general/theme -s "xfcolorize"
+xfconf-query -c xsettings -p /Net/ThemeName -s "adwaita"
+xfconf-query -c xsettings -p /Net/ThemeName -s "xfcolorize"
+xfconf-query -c xfce4-notifyd -p /theme -s "xfcolorize"
+
+echo Restarting panel
+xfce4-panel -r  &> /dev/null
---
+++ xfcolorizer.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import argparse, subprocess, os
+from colorthief import ColorThief
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--image','-i', required=True, help='set image to wallpaperpaper and generate palette from image', nargs='*')
+parser.add_argument('--window','-w', help='set window color and panel color from given value (hex)', nargs='*')
+args = parser.parse_args()
+#img = (args.image[0])
+
+def rgb_to_hex(rgb):
+	return '%02x%02x%02x' % rgb
+
+if args.image[0]:
+	wallpaperpaper=(args.image[0])
+	print ("Generating palette color")
+	color_thief = ColorThief(args.image[0])
+	#dominant_color = color_thief.get_color(quality=1)
+	palette = color_thief.get_palette(color_count=2)
+	hex_palette = list()
+	for i in enumerate(palette):
+		hex_palette.append(rgb_to_hex(palette[i[0]]).upper())
+	if args.window:
+		if args.window[0]:
+			new_palette = list()
+			new_palette.append((args.window[0]).upper())
+			new_palette.append(hex_palette[1])
+			new_palette.append(hex_palette[2])
+			#print("Palette Color")
+			if new_palette[0]==new_palette[1] or new_palette[0]==new_palette[2] or new_palette[1]==new_palette[2]:
+				print("Color can't be same")
+			else :
+				init_file = open( 'wallpaper.sh', 'w' )
+				init_file.writelines( [ '#!/bin/bash\n', 'wallpaperpaper=' + args.image[0] + '&&'] )
+				init_file.writelines( [ 'properties=$(xfconf-query ', '-c ', 'xfce4-desktop ', '-p ', '/backdrop ', '-l ', '| ', 'grep ', '-e ', '"screen.*/monitor.*image-path$" ', '-e ', '"screen.*/monitor.*/last-image$") ', '&& '])
+				init_file.writelines( [ 'for ', 'property ', 'in ', '$properties; ', 'do ', 'xfconf-query ', '-c ', 'xfce4-desktop ', '-p ', '$property ', '-s ', '"$wallpaperpaper"; ', 'done'])
+				init_file.close()
+				subprocess.Popen( [ 'chmod', '+x', './color.sh' ] )
+				subprocess.Popen( [ 'chmod', '+x', './wallpaper.sh' ] )
+				output = subprocess.call(['./color.sh ', new_palette[0], new_palette[1], new_palette[2]])
+				output = subprocess.call("./wallpaper.sh")
+	else:
+		new_palette = list()
+		new_palette = (hex_palette)
+		if new_palette[0]==new_palette[1] or new_palette[0]==new_palette[2] or new_palette[1]==new_palette[2]:
+			print("Color can't be same")
+		else :
+			#print(new_palette)
+			init_file = open( 'wallpaper.sh', 'w' )
+			init_file.writelines( [ '#!/bin/bash\n', 'wallpaperpaper=' + args.image[0] + '&&'] )
+			init_file.writelines( [ 'properties=$(xfconf-query ', '-c ', 'xfce4-desktop ', '-p ', '/backdrop ', '-l ', '| ', 'grep ', '-e ', '"screen.*/monitor.*image-path$" ', '-e ', '"screen.*/monitor.*/last-image$") ', '&& '])
+			init_file.writelines( [ 'for ', 'property ', 'in ', '$properties; ', 'do ', 'xfconf-query ', '-c ', 'xfce4-desktop ', '-p ', '$property ', '-s ', '"$wallpaperpaper"; ', 'done'])
+			init_file.close()
+			subprocess.Popen( [ 'chmod', '+x', './color.sh' ] )
+			subprocess.Popen( [ 'chmod', '+x', './wallpaper.sh' ] )
+			#print(new_palette)
+			output = subprocess.call(['./color.sh', new_palette[0], new_palette[1], new_palette[2]])
+			output = subprocess.call("./wallpaper.sh")
